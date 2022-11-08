159623de8da6cb87b149ebf0e496f973d46799a0
Katariina83 <rikkonenkatariina@gmail.com>
2022-11-08 14:44:34 +0200

    Konsoliohjelma 01: Faker-Nuget paketin käyttörakenna-html tehtävä valmis.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Faker; // käyttämällä tätä riviä, ohjelma ymmärtää että käytetään Faker-kirjastoa, jolloin Faker.Name.FullName()- edessä Faker-referenssi on tarpeeton. 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            var name = Name.FullName(); //tämä on metodi, joka saatiin kun lisättiin NuGet - Faker.NET ohjelmapaketti
+        var city = Address.City();
+        var id = Identification.SocialSecurityNumber();
+        Console.WriteLine($"Hello {name} from {city} id {id}");
+        }
+        
+    }
+}
